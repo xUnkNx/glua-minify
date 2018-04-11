4eab9ca895f69a939b204aa4124f1d1f1c949e66
NiteHawk <n1tehawk@users.noreply.github.com>
2018-04-11 19:34:48 +0200

    Improve on coverage by adding more tests

--- tests.lua
+++ tests.lua
@@ -12,4 +12,20 @@ function test_basics()
 	lu.assertEquals(LuaMinify.AstToString(ast), source)
 end
 
+-- Test invalid syntax and some corner cases, mainly to improve code coverage
+function test_errors()
+	lu.assertErrorMsgContains('Bad symbol `$` in source.',
+		LuaMinify.CreateLuaParser, '$')
+	lu.assertErrorMsgContains('1:1: Unexpected symbol',
+		LuaMinify.CreateLuaParser, '/')
+	lu.assertErrorMsgContains('Unfinished long string.',
+		LuaMinify.CreateLuaParser, '\n[[')
+	lu.assertErrorMsgContains('Invalid Escape Sequence `?`.',
+		LuaMinify.CreateLuaParser, '"\\?"')
+	lu.assertErrorMsgContains('`=` expected.',
+		LuaMinify.CreateLuaParser, 'foobar 4')
+	lu.assertErrorMsgContains('Ident expected.',
+		LuaMinify.CreateLuaParser, 'local function 2')
+end
+
 lu.LuaUnit:run(...)
